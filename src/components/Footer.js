5d1a6f85492dac6b16136748c068a3fa4105f199
JJ-Eshwar <jjeshwar776@gmail.com>
2024-10-16 19:35:16 +0530

    Footer styling was added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,28 @@
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
-      <footer className="bg-gray-800 text-white py-6">
-        <div className="container mx-auto flex flex-col lg:flex-row justify-center items-center px-4">
-          <div className="text-center lg:text-left mb-4 lg:mb-0 lg:mr-4">
-            {/* <span>Created By</span>
+      <div className="Footer bg-gray-800 text-white flex py-4 mt-8 pt-5" >
+        <div className=" bg-gray-800 mx-auto flex  lg:flex-row  px-4">
+          <div className="  text-center lg:text-left mb-4 lg:mb-0 lg:mr-4">
+            <span>Created  By</span>
             <a
               href=""
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-400 hover:text-blue-600 transition-colors duration-300 mx-2"
             >
-              JJ Eshwar
+              { "  "}JJ Eshwar{" "}
             </a>
-            <span className="hidden lg:inline">|</span>
-            <strong className="block lg:inline mt-2 lg:mt-0">
+            <span className=" lg:inline">{"  "}|{ " "}</span>
+            <strong className=" lg:inline mt-2 lg:mt-0">
               Food<span className="text-blue-400">Court</span>
             </strong>
           </div>
           <div className="text-center lg:text-right">
-            <span className="text-gray-400">© {year}</span> */}
+            <span className="text-gray-400">© {year}</span>
           </div>
         </div>
-      </footer>
+      </div>
     );
   };
   
